e4ab8ff7cf1887ffff1146f2d1a21ea09103e8a9
nikiforov.ds2190 <nikiforov.ds2190@M410C2-R55>
2023-05-11 12:25:34 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿public class BlockedCard : Card
+{
+	private string _blockReason;
+    private object CardNumber;
+    private object CardHolder;
+    private object ExpirationDate;
+    private object CVV;
+    private bool IsBlocked;
+
+    public string BlockReason
+	{
+		get { return _blockReason; }
+		set
+		{
+			if (!string.IsNullOrEmpty(value))
+				_blockReason = value;
+			else
+				throw new ArgumentException("Block reason cannot be empty");
+		}
+	}
+
+	void PrintInfo()
+	{
+		Console.WriteLine($"Card number: {CardNumber}");
+		Console.WriteLine($"Card holder: {CardHolder}");
+		Console.WriteLine($"Expiration date: {ExpirationDate}");
+		Console.WriteLine($"CVV: {CVV}");
+		Console.WriteLine($"Status: {(IsBlocked ? "Blocked" : "Active")}");
+		if (IsBlocked)
+		{
+			Console.WriteLine($"Block reason: {BlockReason}");
+		}
+	}
+}
